cb41a27283b7635dd7af71a089690df0b82fc460
Jeff Oberlander <jeffoberlander@gmail.com>
2023-11-30 09:30:21 -0800

    Fixed google query

--- campaigns.py
+++ campaigns.py
@@ -12,20 +12,18 @@ def get_google_campaign_data_totals(_bq_client,daterange):
     sql_query = f"""
         SELECT metrics.campaign_id,
         campaigns.campaign_name,
-        campaigns.campaign_start_date,
-        campaigns.campaign_end_date, 
         sum(metrics_clicks) as clicks,
         sum(metrics_conversions) as conversions,
         sum(metrics_impressions) as impressions,
         sum(metrics_cost_micros) as cost,
         avg(metrics_average_cpc) as cpc, 
-        sum(metrics_conversions) as conversions,
-        sum(metrics_conversions_value)
+        sum(metrics_conversions) as conversions
+        
        
         FROM dataexploration-193817.marketing_data.p_ads_CampaignStats_6687569935 as metrics
         inner join dataexploration-193817.marketing_data.ads_Campaign_6687569935 as campaigns on metrics.campaign_id = campaigns.campaign_id
         WHERE  campaigns.campaign_start_date >= '{date_start}' AND campaigns.campaign_start_date <= '{date_end}'
-        group by metrics.campaign_id,campaigns.campaign_name,campaigns.campaign_start_date,campaigns.campaign_end_date
+        group by metrics.campaign_id,campaigns.campaign_name
     """
     rows_raw = _bq_client.query(sql_query)
     rows = [dict(row) for row in rows_raw]
@@ -35,6 +33,8 @@ def get_google_campaign_data_totals(_bq_client,daterange):
     
     df = pd.DataFrame(rows)
     df["Source"] = ("Google")
+    df["campaign_id"]=df["campaign_id"].astype(str).str.replace(",", "")
+
     return df
 
 
@@ -49,16 +49,14 @@ def get_fb_campaign_data_totals(_bq_client,daterange):
         SELECT 
             campaign_id,
             campaign_name,
-            start_time,
-            end_time,
             sum(clicks) as clicks,
             sum(impressions) as impressions,
             sum(spend) as cost,
             avg(cpc) as cpc, 
             FROM dataexploration-193817.marketing_data.facebook_ads_data
             WHERE  data_date_start >= '{date_start}' AND data_date_start <= '{date_end}'
-            group by campaign_id,campaign_name,start_time,end_time
-            order by campaign_id;    """
+            group by campaign_id,campaign_name;
+            """
 
     rows_raw = _bq_client.query(sql_query)
     rows = [dict(row) for row in rows_raw]
@@ -67,8 +65,8 @@ def get_fb_campaign_data_totals(_bq_client,daterange):
         return pd.DataFrame()
 
     df1 = pd.DataFrame(rows)
-    df1["start_time"] = pd.to_datetime(df1.start_time,utc=True)
-    df1["start_time"] = df1['start_time'].dt.strftime('%Y/%m/%d')
+   # df1["campaign_start_date"] = pd.to_datetime(df1.campaign_start_date,utc=True)
+   # df1["campaign_start_date"] = df1['campaign_start_date'].dt.strftime('%Y/%m/%d')
     
     sql_query = f"""
        SELECT 
@@ -78,8 +76,7 @@ def get_fb_campaign_data_totals(_bq_client,daterange):
         JOIN UNNEST(actions) as a  
         WHERE a.action_type = 'mobile_app_install'
         AND start_time >= '{date_start}' AND start_time <= '{date_end}'
-        group by campaign_id
-        order by campaign_id;    """
+        group by campaign_id;    """
 
     rows_raw = _bq_client.query(sql_query)
     rows = [dict(row) for row in rows_raw]
@@ -91,5 +88,7 @@ def get_fb_campaign_data_totals(_bq_client,daterange):
         merged_df,
         index=['campaign_id'],
         aggfunc={'clicks': np.sum, 'impressions': np.sum, 'cost': np.sum,'cpc': np.sum,'mobile_app_install': np.sum})
+    
+    merged_df["Source"] = ("Facebook")
     return merged_df
 
