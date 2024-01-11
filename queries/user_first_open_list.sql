ac37a0d097308bdf2d613e82a1897b5581d99ebd
Jeff Oberlander <jeffoberlander@gmail.com>
2024-01-11 14:14:35 -0800

    source control on the BigQuery scheduled queries

---
+++ queries/user_first_open_list.sql
@@ -0,0 +1,169 @@
+SELECT
+  DISTINCT (user_pseudo_id),
+  first_open
+FROM (
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-b9d99.analytics_159643920.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND (app_info.id LIKE 'org.curiouslearning.container'
+      OR app_info.id LIKE '%feedthemonster%')
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-afrikaans.analytics_177200876.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-hindi.analytics_174638281.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-brazilian-portuguese.analytics_161789655.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-english.analytics_152408808.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-french.analytics_173880465.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-isixhosa.analytics_180747962.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-kinayrwanda.analytics_177922191.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-oromo.analytics_167539175.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-swahili.analytics_160694316.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-somali.analytics_159630038.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-sepedi.analytics_180755978.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-zulu.analytics_155849122.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-southafricanenglish.analytics_173750850.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE()
+  UNION ALL
+  SELECT
+    user_pseudo_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open
+  FROM
+    `ftm-spanish.analytics_158656398.events_20*`
+  WHERE
+    event_name = 'session_start'
+    AND app_info.id LIKE '%feedthemonster%'
+    AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
+    AND CURRENT_DATE() );
---
+++ queries/users_data.sql
@@ -0,0 +1,39 @@
+SELECT
+    a.user_pseudo_id,
+    a.first_open,
+    a.country,
+    c.la_date as la_date,
+    a.app_language,
+    MAX(a.level) as max_user_level,
+    SAFE_DIVIDE(MAX(a.level), b.max_level) * 100 as gc,
+    b.max_level as max_game_level
+FROM
+    `dataexploration-193817.user_data.all_users` a
+LEFT JOIN
+    (
+        SELECT
+            user_pseudo_id,
+            MIN(event_date) as la_date
+        FROM
+            `dataexploration-193817.user_data.all_users`
+
+        GROUP BY
+            user_pseudo_id
+    ) c
+ON
+    c.user_pseudo_id = a.user_pseudo_id
+LEFT JOIN
+    (
+        SELECT
+            app_language,
+            max_level
+        FROM
+            `dataexploration-193817.user_data.language_max_level`
+        GROUP BY
+            app_language, max_level
+    ) b
+ON
+    b.app_language = a.app_language
+GROUP BY
+    a.user_pseudo_id, a.first_open, a.country, c.la_date, a.app_language, b.max_level
+
