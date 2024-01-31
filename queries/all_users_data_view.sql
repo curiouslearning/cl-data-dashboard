33e7ab2e72c4692048f0f4a8b8c25b2b02c9f00d
Jeff Oberlander <jeffoberlander@gmail.com>
2024-01-31 08:48:34 -0800

    fix dashboard LR due to language event issue

--- queries/all_users_data_view.sql
+++ queries/all_users_data_view.sql
@@ -1,7 +1,7 @@
 SELECT 
    distinct(t1.user_pseudo_id),
    t1.country,
-   t1.app_language, 
+   t2.app_language, 
    t1.app_id,
    ifnull(COALESCE(t2.max_user_level, NULL),0) as max_user_level,
    ifnull(COALESCE(t2.gpc, NULL), 0) as gpc,
@@ -10,5 +10,5 @@ SELECT
    t1.first_open
 
 FROM `dataexploration-193817.user_data.user_first_open_list` AS t1
-LEFT JOIN `dataexploration-193817.user_data.users_data` AS t2
+LEFT JOIN `dataexploration-193817.user_data.la_users_data` AS t2
 ON t1.user_pseudo_id = t2.user_pseudo_id
--- queries/cr_users.sql
+++ queries/cr_users.sql
@@ -8,7 +8,7 @@ SELECT
   PARSE_DATE('%Y%m%d', event_date) as event_date,
   ga.user_pseudo_id AS user_pseudo_id,
   device.language AS device_language,
-  language_params.value.string_value AS app_language,
+  LOWER(REGEXP_EXTRACT(app_params.value.string_value, '[?&]cr_lang=([^&]+)')) AS app_language,
   geo.country AS country,
   (level_params.value.int_value + 1) AS level,  /* CR levels are zero indexed */
   CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open,
@@ -16,13 +16,15 @@ SELECT
 FROM
   `ftm-b9d99.analytics_159643920.events_20*` AS ga,
   UNNEST(event_params) AS level_params,
-  UNNEST(event_params) AS success_params,
-  UNNEST(event_params) AS language_params
+  UNNEST(event_params) AS app_params,
+  UNNEST(event_params) AS success_params
 WHERE
   ga.event_name = 'level_completed'
   AND level_params.key = 'level_number'
   AND success_params.key = 'success_or_failure'
   AND success_params.value.string_value = 'success'
-  AND language_params.key = 'ftm_language'
+  AND app_params.key = 'page_location'
   AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2021-01-01'
   AND CURRENT_DATE()
+ORDER BY
+  event_date DESC
