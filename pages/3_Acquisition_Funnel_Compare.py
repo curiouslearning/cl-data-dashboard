32d240a7bcacb026706354defbbcbe3df5e42994
Jeff Oberlander <jeffoberlander@gmail.com>
2024-03-06 17:00:42 -0800

    comparison funnel

---
+++ pages/3_Acquisition_Funnel_Compare.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import settings
+from rich import print as rprint
+from millify import prettify
+import ui_components as uic
+import ui_widgets as ui
+import users
+
+st.title("Curious Learning Dashboard")
+st.subheader("Acquisition Funne Comparison")
+settings.initialize()
+settings.init_user_list()
+
+uic.engagement_funnel_chart_compare()
