64b6cdf68e18cc3d94a19f7b8d8c1cbb09e2a0ee
Jeff Oberlander <jeffoberlander@gmail.com>
2024-03-19 16:49:07 -0700

    cleaning up BigQuery queries to match latest

--- queries/pre_LA_unity_users_progress.sql
+++ queries/pre_LA_unity_users_progress.sql
@@ -1,90 +1,57 @@
+WITH
+  all_events AS (
+  SELECT
+    user_pseudo_id,
+    "org.curiouslearning.container" AS app_id,
+    CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open,
+    geo.country AS country,
+    LOWER(REGEXP_EXTRACT(app_params.value.string_value, '[?&]cr_lang=([^&]+)')) AS app_language,
+    SUM(CASE
+        WHEN event_name = 'puzzle_completed' THEN 1
+      ELSE
+      0
+    END
+      ) AS puzzle_completed_count,
+    SUM(CASE
+        WHEN event_name = 'selected_level' THEN 1
+      ELSE
+      0
+    END
+      ) AS selected_level_count,
+    SUM(CASE
+        WHEN event_name = 'tapped_start' THEN 1
+      ELSE
+      0
+    END
+      ) AS tapped_start_count
+  FROM
+    `ftm-b9d99.analytics_159643920.events_20*` AS ga,
+    UNNEST(event_params) AS app_params
+  WHERE
+    event_name IN ('tapped_start',
+      'selected_level',
+      'puzzle_completed')
+    AND app_params.key = 'page_location'
+    AND app_params.value.string_value LIKE '%https://feedthemonster.curiouscontent.org%'
+  GROUP BY
+    user_pseudo_id,
+    app_id,
+    first_open,
+    country,
+    app_language )
 SELECT
-  DISTINCT (user_pseudo_id),
-  app_info.id AS app_id,
-  CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open,
-  geo.country AS country,
-  LOWER(REGEXP_EXTRACT(app_info.id, r'feedthemonster(.*)')) AS app_language,
-  "puzzle_completed" AS furthest_event
-FROM (
-  SELECT
-    *
-  FROM
-    `ftm-b9d99.analytics_159643920.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-afrikaans.analytics_177200876.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-hindi.analytics_174638281.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-brazilian-portuguese.analytics_161789655.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-english.analytics_152408808.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-french.analytics_173880465.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-isixhosa.analytics_180747962.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-kinayrwanda.analytics_177922191.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-oromo.analytics_167539175.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-swahili.analytics_160694316.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-somali.analytics_159630038.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-sepedi.analytics_180755978.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-zulu.analytics_155849122.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-southafricanenglish.analytics_173750850.events_20*`
-  UNION ALL
-  SELECT
-    *
-  FROM
-    `ftm-spanish.analytics_158656398.events_20*` )
-CROSS JOIN
-  UNNEST(event_params) AS params
-WHERE
-  event_name = 'GamePlay'
-  AND params.key = 'action'
-  AND params.value.string_value LIKE 'SegmentSuccess%'
-  AND CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) BETWEEN '2021-01-01'
-  AND CURRENT_DATE()
+  user_pseudo_id,
+  app_id,
+  first_open,
+  country,
+  app_language,
+  CASE
+    WHEN puzzle_completed_count > 0 THEN 'puzzle_completed'
+    WHEN selected_level_count > 0 THEN 'selected_level'
+    WHEN tapped_start_count > 0 THEN 'tapped_start'
+  ELSE
+  NULL
+END
+  AS furthest_event
+FROM
+  all_events
