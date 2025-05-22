f7de6562306d0f4d7f4c90b7c759ba9e746e23c6
Jeff Oberlander <joberlander@curiouslearning.org>
2025-05-22 14:42:19 -0700

    add app_version

--- queries/cr_user_progress.sql
+++ queries/cr_user_progress.sql
@@ -1,115 +1,104 @@
-WITH
-  all_events AS (
+WITH all_events AS (
   SELECT
     user_pseudo_id,
-    cr_user_id_params.value.string_value as cr_user_id,
     "org.curiouslearning.container" AS app_id,
     CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open,
     geo.country AS country,
-    LOWER(REGEXP_EXTRACT(app_params.value.string_value, '[?&]cr_lang=([^&]+)')) AS app_language,
-    b.max_level AS max_game_level,
-    MIN(
-      CASE
-        WHEN event_name = 'level_completed' THEN CASE
-        WHEN success_params.key = 'success_or_failure'
-      AND success_params.value.string_value = 'success'
-      AND level_params.key = 'level_number' THEN PARSE_DATE('%Y%m%d', event_date)
-      ELSE
-      NULL
-    END
-      ELSE
-      NULL
-    END
-      ) AS la_date,
-    MAX(
-      CASE
-        WHEN event_name = 'level_completed' THEN CASE
-        WHEN success_params.key = 'success_or_failure'
-      AND success_params.value.string_value = 'success'
-      AND level_params.key = 'level_number' THEN level_params.value.int_value + 1
-      ELSE
-      0
-    END
-      ELSE
-      0
-    END
-      ) AS max_user_level,
-    SUM(CASE
-        WHEN event_name = 'level_completed' AND success_params.key = 'success_or_failure' AND success_params.value.string_value = 'success' AND level_params.key = 'level_number' THEN 1
-      ELSE
-      0
-    END
-      ) AS level_completed_count,
-    SUM(CASE
-        WHEN event_name = 'puzzle_completed' THEN 1
-      ELSE
-      0
-    END
-      ) AS puzzle_completed_count,
-    SUM(CASE
-        WHEN event_name = 'selected_level' THEN 1
-      ELSE
-      0
-    END
-      ) AS selected_level_count,
-    SUM(CASE
-        WHEN event_name = 'tapped_start'  THEN 1
-      ELSE
-      0
-    END
-      ) AS tapped_start_count,
-    SUM(CASE
-        WHEN event_name = 'download_completed' THEN 1
-      ELSE
-      0
-    END
-      ) AS download_completed_count,
-  FROM
-    `ftm-b9d99.analytics_159643920.events_20*` AS a,
-    UNNEST(event_params) AS app_params,
-    UNNEST (event_params) AS success_params,
-    UNNEST(event_params) AS level_params,
-    UNNEST(event_params) AS cr_user_id_params
-  LEFT JOIN (
-    SELECT
-      app_language,
-      max_level
-    FROM
-      `dataexploration-193817.user_data.language_max_level`
-    GROUP BY
-      app_language,
-      max_level ) b
-  ON
-    b.app_language = LOWER(REGEXP_EXTRACT(app_params.value.string_value, '[?&]cr_lang=([^&]+)'))
-  WHERE
-    event_name IN ( 'download_completed',
-      'tapped_start',
-      'selected_level',
-      'puzzle_completed',
-      'level_completed')
-    AND app_params.key = 'page_location'
-    AND app_params.value.string_value LIKE '%https://feedthemonster.curiouscontent.org%'
-    AND device.web_info.hostname LIKE 'feedthemonster.curiouscontent.org%'
-    AND cr_user_id_params.key = 'cr_user_id'
-    AND CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) BETWEEN '2021-01-01'
-  AND CURRENT_DATE()
 
-  GROUP BY
+    -- Extract cr_user_id
+    (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'cr_user_id') AS cr_user_id,
+
+    -- App language
+    LOWER(REGEXP_EXTRACT((SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_location'), r'[?&]cr_lang=([^&]+)')) AS app_language,
+
+    -- Extract app version
+    (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'version_number') AS app_version,
+  
+    -- Level data (only used when event_name = 'level_completed')
+    SAFE_CAST((SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'level_number') AS INT64) AS level_number,
+    (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'success_or_failure') AS success_or_failure,
+
+
+    event_name,
+    event_date
+  FROM
+    `ftm-b9d99.analytics_159643920.events_20*`
+  WHERE
+    event_name IN (
+      'download_completed', 'tapped_start', 'selected_level',
+      'puzzle_completed', 'level_completed'
+    )
+    AND (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_location')
+        LIKE '%https://feedthemonster.curiouscontent.org%'
+    AND device.web_info.hostname LIKE 'feedthemonster.curiouscontent.org%'
+    AND CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) BETWEEN '2021-01-01' AND CURRENT_DATE()
+),
+
+-- Join in max level per language
+joined_events AS (
+  SELECT
+    a.*,
+    b.max_level AS max_game_level
+  FROM all_events a
+  LEFT JOIN (
+    SELECT app_language, max_level
+    FROM `dataexploration-193817.user_data.language_max_level`
+    GROUP BY app_language, max_level
+  ) b
+  ON a.app_language = b.app_language
+),
+
+-- Aggregate per user
+aggregated AS (
+  SELECT
     user_pseudo_id,
     cr_user_id,
-    app_id,
+    app_version,
     first_open,
     country,
     app_language,
-    traffic_source,
-    b.max_level )
+    max_game_level,
+
+    -- Dates and levels only for successful completions
+    MIN(CASE
+      WHEN event_name = 'level_completed'
+        AND success_or_failure = 'success'
+        AND level_number IS NOT NULL
+      THEN PARSE_DATE('%Y%m%d', event_date)
+    END) AS la_date,
+
+    MAX(CASE
+      WHEN event_name = 'level_completed'
+        AND success_or_failure = 'success'
+        AND level_number IS NOT NULL
+      THEN level_number + 1
+    ELSE 0 END) AS max_user_level,
+
+    -- Event counts
+    COUNTIF(event_name = 'level_completed' AND success_or_failure = 'success' AND level_number IS NOT NULL) AS level_completed_count,
+    COUNTIF(event_name = 'puzzle_completed') AS puzzle_completed_count,
+    COUNTIF(event_name = 'selected_level') AS selected_level_count,
+    COUNTIF(event_name = 'tapped_start') AS tapped_start_count,
+    COUNTIF(event_name = 'download_completed') AS download_completed_count
+  FROM joined_events
+  GROUP BY
+    user_pseudo_id,
+    cr_user_id,
+    app_version,
+    first_open,
+    country,
+    app_language,
+    max_game_level
+)
+
+-- Final selection + furthest logic
 SELECT
   user_pseudo_id,
   cr_user_id,
-  app_id,
   first_open,
   country,
   app_language,
+  app_version,
   max_user_level,
   max_game_level,
   la_date,
@@ -119,21 +108,7 @@ SELECT
     WHEN selected_level_count > 0 THEN 'selected_level'
     WHEN tapped_start_count > 0 THEN 'tapped_start'
     WHEN download_completed_count > 0 THEN 'download_completed'
-  ELSE
-  NULL
-END
-  AS furthest_event,
-  SAFE_DIVIDE(MAX(max_user_level),max_game_level) * 100 AS gpc,
-FROM
-  all_events
-GROUP BY
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10
+    ELSE NULL
+  END AS furthest_event,
+  SAFE_DIVIDE(max_user_level, max_game_level) * 100 AS gpc
+FROM aggregated
