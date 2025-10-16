b2af87de599421f5c30da82da8c09c9356d63a83
Jeff Oberlander <joberlander@curiouslearning.org>
2025-10-16 11:40:44 -0700

    add footer with versions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
+import sys
 
 st.set_page_config(layout="wide")
 
@@ -13,3 +14,22 @@ pg = st.navigation(nav)
 add_page_title(pg)
 
 pg.run()
+footer_html = f"""
+<style>
+footer {{
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+    padding: 5px;
+    background-color: #f0f2f6;
+    color: #555;
+    font-size: 0.9em;
+}}
+</style>
+<footer>
+Python {sys.version.split()[0]} | Streamlit {st.__version__}
+</footer>
+"""
+
+st.markdown(footer_html, unsafe_allow_html=True)
